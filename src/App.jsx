ee82b27190e601324b52395d914e9ec36c91c2a1
Milan Chauhan <milanchauhan.development@gmail.com>
2024-01-10 19:34:32 +0530

    Make Copy Button Functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useState, useCallback, useEffect } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
@@ -9,6 +9,9 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false)
 
   const [password, setPassword] = useState("")
+
+  // useRef hook
+  const passwordRef = useRef(null)
 
   const passwordGenerator = useCallback( () => {
     let pass = ""
@@ -27,6 +30,11 @@ function App() {
 
   }, [length, numberAllowed, charAllowed, setPassword])
 
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select()
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
   useEffect(() => {
     passwordGenerator()
   }, [length, numberAllowed, charAllowed, passwordGenerator])
@@ -42,8 +50,10 @@ function App() {
         className='outline-none w-full py-1 px-3'
         placeholder='password'
         readOnly
+        ref={passwordRef}
         />
         <button
+        onClick={copyPasswordToClipboard}
         className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
         >copy</button>
       </div>

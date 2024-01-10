c68a0b8d3286f6ea33a22a39c955540d5df102d5
Milan Chauhan <milanchauhan.development@gmail.com>
2024-01-10 19:15:07 +0530

    Quick Commit

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
 
     for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
-      pass = str.charAt(char)
+      pass += str.charAt(char)
     }
 
     setPassword(pass)
